f4bd48c46be44a7703b74c0c982ec4269289b509
Yeasmin Farzana <yeasminf18@vt.edu>
2022-04-23 14:04:40 -0400

    Initial commit- Working with Vue.js to create my fun fact profile

---
+++ app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      yourName: "Yeasmin Farzana",
+      yourAge: 25,
+      yourImage:
+        "https://www.billboard.com/wp-content/uploads/2021/08/do-not-reuse-feature-bts-billboard-2021-bb12-sunhye-shin-group-1-billboard-1548-1629973334.jpg?w=1024",
+    };
+  },
+  methods: {
+    ageInFiveYears() {
+      return this.yourAge + 5;
+    },
+    outputRandom() {
+      const randomNumber = Math.floor(Math.random() * 2);
+      return randomNumber;
+    },
+  },
+});
+
+app.mount("#assignment");
