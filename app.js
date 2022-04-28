3ad771e897991393d0f25e868afbf4af5590d186
yfarzana <35744734+yfarzana@users.noreply.github.com>
2022-04-28 10:52:59 -0400

    check info

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       yourName: "Yeasmin Farzana",
-      yourAge: 25,
+      yourAge: 21,
       yourImage:
         "https://www.billboard.com/wp-content/uploads/2021/08/do-not-reuse-feature-bts-billboard-2021-bb12-sunhye-shin-group-1-billboard-1548-1629973334.jpg?w=1024",
     };
